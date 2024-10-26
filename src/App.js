1db3511688c621ac1d68986b53ad2dbc009f6e6c
sina <siyam1990@gmail.com>
2024-10-26 11:59:21 +0330

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Products from "./pages/Products";
-import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar.js";
+import Home from "./pages/Home.js";
+import Products from "./pages/Products.js";
+import Cart from "./pages/Cart.js";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </Router>
   );
 }
