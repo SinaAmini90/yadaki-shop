e8199683bcf879003d3a4e70acb27e0f491f9a94
sina <siyam1990@gmail.com>
2024-11-19 19:26:34 +0330

    add news pass and initial js page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home.js";
 import Products from "./pages/Products.js";
 import Cart from "./pages/Cart.js";
 import Footer from "./components/Footer.js";
+import News from "./pages/News.js";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/news" element={<News />} />
       </Routes>
       <Footer />
     </Router>
