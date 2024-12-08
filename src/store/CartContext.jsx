fdf2da5d8c98578d292eaddc6bf4c2012927aa6b
sina <siyam1990@gmail.com>
2024-12-08 14:51:27 +0330

    remove some useless codes

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -8,45 +8,6 @@ const CartContext = createContext({
   removeItem: (id) => {},
 });
 
-// function cartReducer(state, action) {
-//   if (action.type === "ADD_ITEM") {
-//     const existingItemIndex = state.items.findIndex(
-//       (item) => item.id === action.item.id
-//     );
-//     const updatedItems = [...state.items];
-//     if (existingItemIndex > -1) {
-//       const existingItem = state.items[existingItemIndex];
-//       const updatedItem = {
-//         ...existingItem,
-//         quantity: existingItem.quantity + 1,
-//       };
-//       updatedItems[existingItemIndex] = updatedItem;
-//     } else {
-//       updatedItems.push({ ...action.item, quantity: 1 });
-//     }
-//     return { ...state, items: updatedItems };
-//   }
-
-//   if (action.type === "REMOVE_ITEM") {
-//     const existingItemIndex = state.items.findIndex(
-//       (item) => item.id === action.id
-//     );
-//     const updatedItems = [...state.item];
-//     const existingItem = state.items[existingItemIndex];
-//     if (existingItem.quantity > 1) {
-//       const updatedItem = {
-//         ...existingItem,
-//         quantity: existingItem.quantity - 1,
-//       };
-//       updatedItems[existingItemIndex] = updatedItem;
-//     } else {
-//       updatedItems.splice(existingItemIndex);
-//     }
-//     return { ...state, items: updatedItems };
-//   }
-//   return state;
-// }
-
 function cartReducer(state, action) {
   switch (action.type) {
     case "ADD_ITEM": {
@@ -103,7 +64,6 @@ export function CartContextProvider({ children }) {
   function removeItem(id) {
     dispach({ type: "REMOVE_ITEM", id });
   }
-  console.log(cartContext);
   return (
     <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
   );
