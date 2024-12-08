e1e67148255f88dbaa1efafa71d42959e402aa77
sina <siyam1990@gmail.com>
2024-12-08 14:52:25 +0330

    add to button for remove and add items

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,19 +5,34 @@ import CartContext from "../store/CartContext.jsx";
 
 export default function Product({ ...props }) {
   const cartCtx = useContext(CartContext);
+  console.log("items", cartCtx.items);
 
   function handleAddProduct(product) {
     cartCtx.addItem(product);
   }
+  function handleRemoveProduct(id) {
+    cartCtx.removeItem(id);
+  }
+  const itemQuantity = cartCtx.items.find(
+    (item) => item.id === props.id
+  )?.quantity;
   return (
     <li className="product-card" key={props.id}>
       <img src={props.image} alt={props.name} className="product-image" />
       <h2 className="product-name">{props.name}</h2>
       <p className="description">More Description</p>
       <p className="product-price">${props.price.toFixed(2)}</p>
-      <Button onClick={() => handleAddProduct(props)} cssClass="add-to-cart">
-        Add to Cart
-      </Button>
+      {itemQuantity ? (
+        <div>
+          <Button onClick={() => handleAddProduct(props)}>+</Button>
+          <span>{itemQuantity || 0}</span>
+          <Button onClick={() => handleRemoveProduct(props.id)}>-</Button>
+        </div>
+      ) : (
+        <Button onClick={() => handleAddProduct(props)} cssClass="add-to-cart">
+          Add to Cart
+        </Button>
+      )}
     </li>
   );
 }
