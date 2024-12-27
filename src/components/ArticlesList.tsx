e15dd061ef1f94a6daa71cb9ace8aa0597c1b038
sina <siyam1990@gmail.com>
2024-12-27 12:42:27 +0330

    creaate initial news componente to use in page

---
+++ src/components/ArticlesList.tsx
@@ -0,0 +1,67 @@
+import "./ArticlesList.css";
+import newsData from "../data/data";
+import { useState } from "react";
+
+export default function ArticlesDetailed() {
+  const [searchedData, setSearchedData] = useState(newsData);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query = e.target.value.toLowerCase();
+    setSearchQuery(query);
+    const filteredData = newsData.filter(
+      (news) =>
+        news.title.toLowerCase().includes(query) ||
+        news.summary.toLowerCase().includes(query)
+    );
+    setSearchedData(filteredData);
+  };
+  const highlightText = (text: string, query: string) => {
+    if (!query) return text; // If there's no query, return the original text
+
+    const parts = text.split(new RegExp(`(${query})`, "gi"));
+    console.log(parts);
+
+    return parts.map((part, index) =>
+      part.toLowerCase() === query.toLowerCase() ? (
+        <span key={index} className="highlight">
+          {part}
+        </span>
+      ) : (
+        part
+      )
+    );
+  };
+  return (
+    <section id="article-container" className="article-container">
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => handleSearch(e)}
+        placeholder="search..."
+      ></input>
+      <div className="news-section">
+        {searchedData.length === 0 ? (
+          <p className="no-results-message">Nothing matches "{searchQuery}"</p>
+        ) : (
+          searchedData.map((news, index) => (
+            <article key={index} className="news-card">
+              <img src={news.image} alt={news.title} className="news-image" />
+              <div>
+                <h3 className="news-title">
+                  {highlightText(news.title, searchQuery)}
+                </h3>
+                <p className="news-summary">
+                  {highlightText(news.summary, searchQuery)}
+                </p>
+                <a href="/news" className="read-more-link">
+                  Read More...
+                </a>
+              </div>
+            </article>
+          ))
+        )}
+      </div>
+    </section>
+  );
+}
