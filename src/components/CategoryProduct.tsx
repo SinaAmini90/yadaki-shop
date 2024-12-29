40ccbc694786199b9ea75484aaf30f5120d3f5ad
sina <siyam1990@gmail.com>
2024-12-29 13:01:28 +0330

    add navigation and state to handle product category handeling in home page

--- src/components/CategoryProduct.tsx
+++ src/components/CategoryProduct.tsx
@@ -1,17 +1,25 @@
 import "./CategoryProduct.css";
+import { useNavigate } from "react-router-dom";
 import { categoryData } from "../data/data.js";
 
 export default function CategoryProduct() {
-  console.log(categoryData);
+  const navigate = useNavigate();
+  const handleCategoryClick = (category: string) => {
+    navigate("/products", { state: { selectedCategory: category } });
+  };
   return (
     <div>
       <div className="products-category-container">
         {categoryData.map((category) => {
           return (
-            <a href="/products" className="product">
-              <img src={category.image} alt="category 1" />
+            <div
+              key={category.title}
+              className="product-category"
+              onClick={() => handleCategoryClick(category.title)}
+            >
+              <img src={category.image} alt={category.title} />
               <h3>{category.title}</h3>
-            </a>
+            </div>
           );
         })}
       </div>
